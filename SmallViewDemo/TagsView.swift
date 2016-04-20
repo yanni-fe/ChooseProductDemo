dbaec338b681776e18695f3903f8a086a4d7225e
yupengyang <yupengyang@outlook.com>
2016-04-20 11:05:35 +0800

    add git ignore

---
+++ SmallViewDemo/TagsView.swift
@@ -0,0 +1,13 @@
+//
+//  TagsView.swift
+//  SmallViewDemo
+//
+//  Created by Yu Pengyang on 4/14/16.
+//  Copyright © 2016 Caishuo. All rights reserved.
+//
+
+import Cocoa
+
+class TagsView: UIView {
+
+}
